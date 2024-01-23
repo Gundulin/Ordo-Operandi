55be3d63b4d7b041a699a2321634012f87b90e5a
Austen <cheesit@gmail.com>
2024-01-23 14:18:33 -0800

    built without base-href for test

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RGPZI2H6.mjs";import"./chunk-QV7A4SIG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-GFRHB7PH.mjs";import"./chunk-QV7A4SIG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
