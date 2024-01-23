56ecf9b8286bb79fc017642c7e1427637e2924fd
Austen <cheesit@gmail.com>
2024-01-23 11:52:04 -0800

    frame footer in basic working condition

--- src/app/frame/frame-footer/frame-footer.component.ts
+++ src/app/frame/frame-footer/frame-footer.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-frame-footer',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './frame-footer.component.html',
   styleUrl: './frame-footer.component.css'
 })
 export class FrameFooterComponent {
+
+  @Output()
+  prevPhase: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  nextPhase: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  unitProfiles: EventEmitter<any> = new EventEmitter();
+
+  protected onPrevPush() {
+    this.prevPhase.emit();
+  }
+
+  protected onNextPush() {
+    this.nextPhase.emit();
+  }
+
+  protected onUnitProfilesPush() {
+    this.unitProfiles.emit();
+  }
 
 }
